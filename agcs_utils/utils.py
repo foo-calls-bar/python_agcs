0c867efa770d924021c2b15befa94a4203d23211
Ryan Kaiser <ryandkaiser@gmail.com>
2016-07-01 16:15:33 -0500

    v3.0

---
+++ agcs_utils/utils.py
@@ -0,0 +1,113 @@
+import socket
+import os
+import pwd, grp
+import random
+from string import ascii_letters as _letters
+from string import digits as _digits
+
+__all__ = [
+    'random_string', 'num_cpus', 'get_ip',
+    'Host', 'Puser', 'Pgroup',
+]
+
+def random_string(length=12, chars=_letters+_digits):
+    return ''.join(
+        random.choice(chars) for i in range(length)
+    )
+
+def num_cpus():
+    try:
+        from multiprocessing import cpu_count
+        return cpu_count()
+    except NotImplementedError as e:
+        print(e.message)
+        return 1
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+class Host(object):
+
+    def __init__(self, ip=None):
+        self._ip = ip and ip or get_ip()
+        self._name, self._cnames, self._ips = (
+            socket.gethostbyaddr(self._ip)
+        )
+
+    @property
+    def cnames(self):
+        return self._cnames
+
+    @property
+    def ips(self):
+        return self._ips
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def ip(self):
+        return self._ip
+
+    @ip.setter
+    def ip(self, addr):
+        self._name =  socket.gethostbyaddr(addr)
+        self._ip = addr
+
+
+class Puser(object):
+
+    def __init__(self, uid=None, name=None):
+        if uid:
+            self._pwd = pwd.getpwuid(uid)
+        elif name:
+            self._pwd = pwd.getpwnam(name)
+        else:
+            self._pwd = pwd.getpwuid(os.getuid())
+
+    @property
+    def name(self):
+        return self._pwd.pw_name
+
+    @property
+    def id(self):
+        return self._pwd.pw_uid
+
+    @property
+    def home(self):
+        return self._pwd.pw_dir
+
+    @property
+    def shell(self):
+        return self._pwd.pw_shell
+
+
+class Pgroup:
+
+    def __init__(self, gid=None, name=None):
+        if gid:
+            self._grp = grp.getgrgid(gid)
+        elif name:
+            self._grp = grp.getgrnam(name)
+        else:
+            self._grp = grp.getgrgid(os.getgid())
+
+    @property
+    def name(self):
+        return self._grp.gr_name
+
+    @property
+    def id(self):
+        return self._grp.gr_gid
+
+    @property
+    def mem(self):
+        return self._grp.gr_mem
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='agcs_utils',
-      version='2.0',
+      version='3.0',
       description='Python3 user and utils module',
       author='Ryan Kaiser',
       author_email='ryank@alphageekcs.com',
