7b4e10b0cff47927df6a94f516ddb46500e76ba2
Ryan Kaiser <ryandkaiser@gmail.com>
2016-07-01 16:31:49 -0500

    add download_url

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='agcs_utils',
-      version='3.0',
-      description='Python3 user and utils module',
-      author='Ryan Kaiser',
-      author_email='ryank@alphageekcs.com',
-      packages=['agcs_utils']
+    version='3.0',
+    description='Python3 user and utils module',
+    author='Ryan Kaiser',
+    author_email='ryank@alphageekcs.com',
+    download_url='https://github.com/ryankaiser/python_agcs/archive/v3.tar.gz',
+    packages=['agcs_utils']
 )
 
